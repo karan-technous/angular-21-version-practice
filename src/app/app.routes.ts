e73c0cc3b016e89fd898c4422ff0940dddccaffa
karan-technous <karan@technous.in>
2026-02-12 12:39:12 +0530

    add routing for testing lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'aria',
+    loadComponent: () => import('./angular-aria/angular-aria').then((m) => m.AngularAriaComponent),
+  },
+  {
+    path: 'reactive-forms',
+    loadComponent: () =>
+      import('./app-reactice-forms/app-reactice-forms').then((m) => m.AppReacticeForms),
+  },
+  {
+    path: 'signal-forms',
+    loadComponent: () =>
+      import('./app-signal-forms/app-signal-forms').then((m) => m.AppSignalForms),
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,13 @@ import { Component, signal } from '@angular/core';
 import { AppSignalForms } from './app-signal-forms/app-signal-forms';
 import { AppReacticeForms } from './app-reactice-forms/app-reactice-forms';
 import { AngularAriaComponent } from './angular-aria/angular-aria';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports:[AppSignalForms,AppReacticeForms, AngularAriaComponent]
+  imports:[AppSignalForms,AppReacticeForms, AngularAriaComponent, RouterOutlet, RouterLink]
 })
 export class App {
   protected readonly title = signal('my-angular-21-app');
